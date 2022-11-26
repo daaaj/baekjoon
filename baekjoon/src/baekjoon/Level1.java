3d3766f73a9e47d0686f2150d93085c62f948229
정다정 <@LAPTOP-12B5GUUU>
2022-11-27 00:01:24 +0900

    수정

--- baekjoon/src/baekjoon/Level1.java
+++ baekjoon/src/baekjoon/Level1.java
@@ -2,12 +2,13 @@ package baekjoon;
 
 import java.util.Scanner;
 
-public class Main {
+public class Level1 {
 	
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
+		// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
 		
+		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
